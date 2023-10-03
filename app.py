480ed05c98b285b5669735e08dfb1cd26bdc253c
manosmin <manosmin@hotmail.com>
2023-10-04 02:03:59 +0300

    added try catch blocks

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import requests
 from pymongo import MongoClient
-import json
-import uvicorn
 
 app = FastAPI()
 
@@ -12,47 +10,57 @@ db = mongo_client.news
 
 @app.get('/headlines')
 def get_trending():
+    try:
+        # API Query for the most recent news headlines
+        news_api_key = '193223099a8045a2b2e24e49d4788aa4'
+        news_api_url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={news_api_key}'
+        response = requests.get(news_api_url)
+        response.raise_for_status()  # Raise an exception if there's an HTTP error
+        data = response.json()
 
-    # API Query for the most recent news headlines
-    news_api_key = '193223099a8045a2b2e24e49d4788aa4'
-    news_api_url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={news_api_key}'
-    response = requests.get(news_api_url)
-    data = response.json()
+        # Insert the data into MongoDB
+        insert_into_mongodb(data)
 
-    # Insert the data into MongoDB
-    insert_into_mongodb(data)
-
-    return data
+        return data
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail="Failed to fetch data from News API")
 
 @app.get('/headlines/{user_q}')
 def get_trending_with_query(user_q: str):
-    
-    # API Query for the most recent news headlines for user's specified query
-    news_api_key = '193223099a8045a2b2e24e49d4788aa4'
-    news_api_url = f'https://newsapi.org/v2/top-headlines?q={user_q}&apiKey={news_api_key}'
-    response = requests.get(news_api_url)
-    data = response.json()
+    try:
+        # API Query for the most recent news headlines for user's specified query
+        news_api_key = '193223099a8045a2b2e24e49d4788aa4'
+        news_api_url = f'https://newsapi.org/v2/top-headlines?q={user_q}&apiKey={news_api_key}'
+        response = requests.get(news_api_url)
+        response.raise_for_status()  # Raise an exception if there's an HTTP error
+        data = response.json()
 
-    # Insert the data into MongoDB
-    insert_into_mongodb(data)
+        # Insert the data into MongoDB
+        insert_into_mongodb(data)
 
-    return data
+        return data
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail="Failed to fetch data from News API")
 
 def insert_into_mongodb(data):
-    # Access the 'news' collection in MongoDB
-    news_collection = db.news
+    try:
+        # Access the 'news' collection in MongoDB
+        news_collection = db.news
 
-    # Iterate through the articles and try to update with upsert
-    for article in data.get('articles', []):
-        # Define a unique filter based on the 'url' field
-        unique_filter = {'url': article['url']}
-        
-        # Use the update_one method with upsert=True to insert or update
-        news_collection.update_one(
-            filter=unique_filter,
-            update={'$set': article},
-            upsert=True
-        )
+        # Iterate through the articles and try to update with upsert
+        for article in data.get('articles', []):
+            # Define a unique filter based on the 'url' field
+            unique_filter = {'url': article['url']}
+            
+            # Use the update_one method with upsert=True to insert or update
+            news_collection.update_one(
+                filter=unique_filter,
+                update={'$set': article},
+                upsert=True
+            )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Failed to insert data into MongoDB")
 
 if __name__ == '__main__':
+    import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=5000)
