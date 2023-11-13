08459465dbd5f1aa9dc091e452c57a76f624e59d
manosmin <manosmin@hotmail.com>
2023-11-13 16:53:06 +0200

    moved script

--- static/main.js
+++ static/main.js
@@ -1,69 +1,65 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchInput = document.getElementById('searchInput');
-    const countrySelect = document.getElementById('countrySelect');
-    const categorySelect = document.getElementById('categorySelect');
-    const sourcesSelect = document.getElementById('sourcesSelect');
-    const pageSizeSelect = document.getElementById('pageSizeSelect');
-    const searchButton = document.getElementById('searchButton');
-    const newsList = document.getElementById('newsList');
+const searchInput = document.getElementById("searchInput");
+const countrySelect = document.getElementById("countrySelect");
+const categorySelect = document.getElementById("categorySelect");
+const sourcesSelect = document.getElementById("sourcesSelect");
+const pageSizeSelect = document.getElementById("pageSizeSelect");
+const searchButton = document.getElementById("searchButton");
 
-    searchButton.addEventListener('click', () => {
-        const query = searchInput.value.trim();
-        const country = countrySelect.value;
-        const category = categorySelect.value;
-        const sources = sourcesSelect.value;
-        const pageSize = parseInt(pageSizeSelect.value);
-        
-        // Send the query to the backend
-        fetch(`/headlines/?query=${query}&country=${country}&category=${category}&sources=${sources}&pageSize=${pageSize}`)
-            .then((response) => response.json())
-            .then((data) => {
-                // Clear previous news
-                newsList.innerHTML = '';
 
-                if (data.articles && data.articles.length > 0) {
-                    // Display news headlines
-                    data.articles.forEach((article) => {
-                        const articleElement = document.createElement('div');
-                    articleElement.className = 'article';
 
-                    const titleElement = document.createElement('h4');
-                    titleElement.textContent = article.title;
+const fetchNews = async () => {
+    const query = searchInput.value.trim();
+    const country = countrySelect.value;
+    const category = categorySelect.value;
+    const sources = sourcesSelect.value;
+    const pageSize = parseInt(pageSizeSelect.value);
+    console.log("Fetching News...");
+    
+    var url = `/headlines/?query=${query}&country=${country}&category=${category}&sources=${sources}&pageSize=${pageSize}`;
+    var req = new Request(url);
 
-                    const descriptionElement = document.createElement('p');
-                    descriptionElement.textContent = article.description;
+    let a = await fetch(req);
+    
+    let response = await a.json();
 
-                    const sourceIdElement = document.createElement('p');
-                    sourceIdElement.textContent = article.source.id;
+    console.log(JSON.stringify(response));
+    let str = "";
+    resultCount.innerHTML = response.totalResults + " Results";
+    for (let item of response.articles) {
+      if (item.title != '[Removed]') {
+      const publishedDate = new Date(item.publishedAt).toLocaleString(
+        "en-GB",
+        { timeZone: "Europe/Athens" }
+      );
+      if (item.description == null) { 
+        item.description = '' 
+      }
+      if (item.source.id == null) { 
+        item.source.id = '' 
+      }
+      if (item.urlToImage == null) { 
+        item.urlToImage = 'static/placeholder.jpg' 
+      }
+      str =
+        str +
+        `<div class="col-sm d-flex">
+          <div class="card card-body flex-fill my-2" style="width: 20rem">
+              <img src="${item.urlToImage}" class="card-img-top" alt="...">
+              <div class="card-body">
+              <h5 class="card-title">${item.title}</h5>
+              <p class="card-text">${publishedDate}</p>
+              <p class="card-text">${item.source.id}</p>
+              <p class="card-text">${item.description}</p>
+              <a href="${item.url}" class="link-offset-2 link-underline link-underline-opacity-0" target="_blank" >Read More</a>
+              </div>
+          </div>
+        </div>`;
+    }
+  }
+    document.querySelector(".content").innerHTML = str;
+  };
 
-                    const urlElement = document.createElement('a');
-                    urlElement.textContent = 'Read More';
-                    urlElement.href = article.url;
-                    urlElement.target = '_blank'; // Open link in a new tab
 
-                    const publishedAtElement = document.createElement('p');
-                    const publishedDate = new Date(article.publishedAt);
-                    publishedAtElement.textContent = publishedDate.toLocaleString('en-GB', { timeZone: 'Europe/Athens' });
+document.addEventListener("DOMContentLoaded", () => { fetchNews(); });
 
-                    articleElement.appendChild(titleElement);
-                    articleElement.appendChild(publishedAtElement);
-                    articleElement.appendChild(sourceIdElement);
-                    articleElement.appendChild(descriptionElement);
-                    articleElement.appendChild(urlElement);
-
-                    const cardItemElement = document.createElement('div');
-                    cardItemElement.className = 'card-body';
-
-                    cardItemElement.appendChild(articleElement);
-                    newsList.appendChild(cardItemElement);
-                    });
-                } else {
-                    newsList.innerHTML = 'No results found.';
-                }
-            })
-            .catch((error) => {
-                console.error('Error fetching news data:', error);
-                newsList.innerHTML = 'An error occurred while fetching data.';
-            });
-    });
-});
+searchButton.addEventListener("click", () => { fetchNews(); });
