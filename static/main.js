e75aec30dc88cdfcf97e14cc177f50b1f2cb57f9
manosmin <manosmin@hotmail.com>
2023-11-15 00:55:11 +0200

    create alerts

--- static/main.js
+++ static/main.js
@@ -5,40 +5,37 @@ const sourcesSelect = document.getElementById("sourcesSelect");
 const pageSizeSelect = document.getElementById("pageSizeSelect");
 const searchButton = document.getElementById("searchButton");
 
-
-
 const fetchNews = async () => {
-    const query = searchInput.value.trim();
-    const country = countrySelect.value;
-    const category = categorySelect.value;
-    const sources = sourcesSelect.value;
-    const pageSize = parseInt(pageSizeSelect.value);
-    console.log("Fetching News...");
-    
-    var url = `/headlines/?query=${query}&country=${country}&category=${category}&sources=${sources}&pageSize=${pageSize}`;
-    var req = new Request(url);
+  const query = searchInput.value.trim();
+  const country = countrySelect.value;
+  const category = categorySelect.value;
+  const sources = sourcesSelect.value;
+  const pageSize = parseInt(pageSizeSelect.value);
+  console.log("Fetching News...");
 
-    let a = await fetch(req);
-    
-    let response = await a.json();
+  var url = `/headlines/?query=${query}&country=${country}&category=${category}&sources=${sources}&pageSize=${pageSize}`;
+  var req = new Request(url);
 
-    console.log(JSON.stringify(response));
-    let str = "";
-    resultCount.innerHTML = response.totalResults + " Results";
-    for (let item of response.articles) {
-      if (item.title != '[Removed]') {
-      const publishedDate = new Date(item.publishedAt).toLocaleString(
-        "en-GB",
-        { timeZone: "Europe/Athens" }
-      );
-      if (item.description == null) { 
-        item.description = '' 
+  let a = await fetch(req);
+
+  let response = await a.json();
+
+  console.log(JSON.stringify(response));
+  let str = "";
+  resultCount.innerHTML = response.totalResults + " Results";
+  for (let item of response.articles) {
+    if (item.title != "[Removed]") {
+      const publishedDate = new Date(item.publishedAt).toLocaleString("en-GB", {
+        timeZone: "Europe/Athens",
+      });
+      if (item.description == null) {
+        item.description = "";
       }
-      if (item.source.id == null) { 
-        item.source.id = '' 
+      if (item.source.id == null) {
+        item.source.id = "";
       }
-      if (item.urlToImage == null) { 
-        item.urlToImage = 'static/placeholder.jpg' 
+      if (item.urlToImage == null) {
+        item.urlToImage = "static/placeholder.jpg";
       }
       str =
         str +
@@ -56,10 +53,66 @@ const fetchNews = async () => {
         </div>`;
     }
   }
-    document.querySelector(".content").innerHTML = str;
-  };
+  document.querySelector(".content").innerHTML = str;
+};
 
+document.addEventListener("DOMContentLoaded", () => {
+  fetchNews();
+});
 
-document.addEventListener("DOMContentLoaded", () => { fetchNews(); });
+searchButton.addEventListener("click", () => {
+  if (pageSizeSelect.value === "") {
+    showPageSizeAlert(1);
+  } else if (parseInt(pageSizeSelect.value) > 100 || pageSizeSelect.value < 100) {
+    showPageSizeAlert(2);
+  } else if (
+    searchInput.value === "" &&
+    countrySelect.value === "" &&
+    categorySelect.value === "" &&
+    sourcesSelect.value === ""
+  ) {
+    showPageSizeAlert(3);
+  } else {
+    fetchNews();
+  }
+});
 
-searchButton.addEventListener("click", () => { fetchNews(); });
+function showPageSizeAlert(type) {
+  const alertDiv = document.createElement("div");
+  alertDiv.className = "alert alert-warning alert-dismissible fade show";
+  alertDiv.setAttribute("role", "alert");
+  if (type == 1) {
+    alertDiv.innerHTML = `
+    <strong>Error!</strong> You should select a page size.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  `;
+  } else if (type == 2) {
+    alertDiv.innerHTML = `
+    <strong>Error!</strong> Page size must be maximum 100.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  `;
+  } else {
+    alertDiv.innerHTML = `
+    <strong>Error!</strong> You should select at least one search option.
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  `;
+  }
+
+  const alertContainer = document.getElementById("alertContainer");
+  alertContainer.appendChild(alertDiv);
+
+  // Remove the alert after some seconds
+  setTimeout(() => {
+    alertDiv.classList.remove("show");
+    alertDiv.classList.add("fade");
+    setTimeout(() => {
+      alertContainer.removeChild(alertDiv);
+    }, 1000);
+  }, 3000);
+}
